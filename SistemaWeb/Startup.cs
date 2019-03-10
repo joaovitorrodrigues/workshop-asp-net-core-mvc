cc17a7a57f9374533d297db6d94550e5410e5845
João Vitor <jaozitus@gmail.com>
2019-03-10 20:04:24 -0300

    SellerService and basic FindAll

--- SistemaWeb/Startup.cs
+++ SistemaWeb/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SistemaWeb.Models;
 using SistemaWeb.Data;
+using SistemaWeb.Services;
 
 namespace SistemaWeb
 {
@@ -40,6 +41,7 @@ namespace SistemaWeb
             services.AddDbContext<SistemaWebContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SistemaWebContext"),builder => builder.MigrationsAssembly("SistemaWeb")));
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
