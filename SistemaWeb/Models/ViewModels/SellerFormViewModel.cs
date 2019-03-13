4c56173512be8f983ef8789aaf2cdf080e9f3aa1
João Vitor <jaozitus@gmail.com>
2019-03-13 16:27:32 -0300

    SellerFormViewModel and Department select component

---
+++ SistemaWeb/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaWeb.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+
+    }
+}
--- SistemaWeb/Services/DepartmentService.cs
+++ SistemaWeb/Services/DepartmentService.cs
@@ -6,24 +6,18 @@ using System.Threading.Tasks;
 
 namespace SistemaWeb.Services
 {
-    public class SellerService
+    public class DepartmentService
     {
         private readonly SistemaWebContext _context;
 
-        public SellerService(SistemaWebContext context)
+        public DepartmentService(SistemaWebContext context)
         {
             _context = context;
         }
 
-        public List<Seller> FindAll()
+        public List<Department> FindAll()
         {
-            return _context.Seller.ToList();
-        }
-
-        public void Insert(Seller obj)
-        {
-            _context.Add(obj);
-            _context.SaveChanges();
+            return _context.Department.OrderBy(x => x.Name).ToList();
         }
     }
 }
