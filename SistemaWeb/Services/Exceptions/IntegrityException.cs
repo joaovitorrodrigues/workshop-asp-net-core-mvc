19e0fdcd0523fe0a0a594c91233c40bc4122a302
João Vitor <jaozitus@gmail.com>
2019-03-16 19:56:43 -0300

    Exception handling for delete (referential integrity)

---
+++ SistemaWeb/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SistemaWeb.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
