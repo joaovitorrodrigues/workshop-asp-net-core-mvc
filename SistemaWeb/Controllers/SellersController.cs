acb25e5dcf9c4d13eae638d720a8d97a85f88e84
João Vitor <jaozitus@gmail.com>
2019-03-16 17:12:15 -0300

    Seller details and eager loading

--- SistemaWeb/Controllers/SellersController.cs
+++ SistemaWeb/Controllers/SellersController.cs
@@ -55,6 +55,22 @@ namespace SistemaWeb.Controllers
             return View(obj);
         }
 
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Delete(int id)
